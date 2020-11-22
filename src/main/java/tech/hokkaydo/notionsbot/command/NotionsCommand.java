01821886cb67d65daaca7a208ac7d633279b5a94
Hokkaydo <42278077+Hokkaydo@users.noreply.github.com>
2020-11-22 19:15:42 +0100

    Update build.gradle (remove useless dependency) + update imports (Charset import)

--- src/main/java/tech/hokkaydo/notionsbot/command/NotionsCommand.java
+++ src/main/java/tech/hokkaydo/notionsbot/command/NotionsCommand.java
@@ -21,6 +21,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
